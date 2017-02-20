991da0308719f71ff940432bd16ccb8a18d765d3
Jonny Holupchinski <jonholup@gmail.com>
2017-02-19 20:29:39 -0600

    fade almost working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,86 +13,54 @@ $(document).ready(function(){
       currentPerson = data.phirephiters[currentPersonIndex]; // set currentPerson to first object
       appendDOM(currentPerson); // display 1st person on success
       generateStatusBar(personArray);
-
     }
   });
 
   /////// BUTTON LISTENERS ///////
   $('#nextButton').on('click', function(){
-    console.log('next');
-    console.log(currentPerson);
+    $('#dataContainer').empty();
     if (currentPersonIndex == personArray.length - 1){
       currentPersonIndex = 0;
-      selectStatus();
-      currentPerson = personArray[currentPersonIndex];
-      console.log(currentPerson);
-      appendDOM(currentPerson);
     } else {
       currentPersonIndex++;
-      selectStatus();
-      currentPerson = personArray[currentPersonIndex];
-      appendDOM(currentPerson);
     }
-
+    selectStatus();
+    currentPerson = personArray[currentPersonIndex];
+    appendDOM(currentPerson);
   });
 
   $('#backButton').on('click', function(){
-    console.log('back');
+    $('#dataContainer').empty();
     if (currentPersonIndex == 0) {
       currentPersonIndex = personArray.length - 1;
-      selectStatus();
-      currentPerson = personArray[currentPersonIndex];
-      appendDOM(currentPerson);
-
     } else {
       currentPersonIndex--;
-      selectStatus();
-      currentPerson = personArray[currentPersonIndex];
-      appendDOM(currentPerson);
     }
-
+    selectStatus();
+    currentPerson = personArray[currentPersonIndex];
+    appendDOM(currentPerson);
   });
-
 });
 
 /////// FUNCTIONS ///////
 function appendDOM(person){
   $('#dataContainer').empty();
   $('#dataContainer').append('<div class="person"></div>');
-  var $el = $('#dataContainer').children().fadeIn('slow');
-  $el.append('<h2> Name: ' + person.name + '</h2>');
-  $el.append('<h2> Git User: ' + person.git_username + '</h2>');
-  $el.append('<h2> Shoutout: ' + person.shoutout + '</h2>');
+  var $el = $('#dataContainer').children();
+  $el.append('<h2> Name: ' + person.name + '</h2>').hide().fadeIn();
+  $el.append('<h2> Git User: ' + person.git_username + '</h2>').hide().fadeIn();
+  $el.append('<h2> Shoutout: ' + person.shoutout + '</h2>').hide().fadeIn();
 
 }
 
 function generateStatusBar(array){
   for (var i = 0; i < array.length; i++) {
     $('#carouselStatus').append('<div data-id=' + i + '>' + (i + 1) + '</div>');
-    // $('#carouselStatus:first-child').
     $('[data-id=' + '"' + currentPersonIndex + '"' + ']').addClass('selected');
-
-
   }
 }
 
 function selectStatus(){
   $('#carouselStatus > div').removeClass(); //remove last selected
   $('[data-id=' + '"' + currentPersonIndex + '"' + ']').addClass('selected');
-
 }
-
-
-// NOTE: todo:
-// working function appendDOM(person){
-//   $('#dataContainer').empty();
-//   $('#dataContainer').append('<div class="person"></div>');
-//   var $el = $('#dataContainer').children();
-//   $el.append('<h2> Name: ' + person.name + '</h2>');
-//   $el.append('<h2> Git User: ' + person.git_username + '</h2>');
-//   $el.append('<h2> Shoutout: ' + person.shoutout + '</h2>');
-// }
-
-// function selectStatus(){
-//   $('#carouselStatus > div').removeAttr('id'); //remove last selected
-//   $('[data-id=' + '"' + currentPersonIndex + '"' + ']').attr('id', 'selected');
